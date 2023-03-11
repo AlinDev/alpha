e69b041b81bd0924a831818e27a4789b5b4ac309
Alin <boaldeveloper@gmail.com>
2023-03-11 17:11:42 +0200

    add web packages and infrastructure libs

---
+++ src/mockup/mockup_accountHistory_vs1.js
@@ -0,0 +1,55 @@
+export const mockup_data = {
+  "address": "erd1gy7str3lx9elhnmlfcumd3g4fzryk885dljreqmjxqfwdyqfgufqep60qr",
+  "network": "devnet",
+  "versions": [
+    {
+      "balance": "1000000000000000000",
+      "nonce": 0,
+      "tx_hash": "fe49445b5515907750e237b3a2ce7186e89bb7d33ba1081ea2db54d476510fc7",
+      "version": 2,
+      "tokens": [
+        {"token": "EGLD",
+        "name": "Native Token",
+        "balance": "2222"
+        } ,
+         {"token": "UTK-xx",
+          "name": "Utrust (UTK) Token",
+         "balance": "11111"
+          }
+       ]
+    },
+    {
+      "balance": "2000000000000000000",
+      "nonce": 0,
+      "tx_hash": "fe49445b5515907750e237b3a2ce7186e89bb7d33ba1081ea2db54d476510fc7",
+      "version": 1,
+      "tokens": [
+        {"token": "EGLD",
+        "name": "Native Token",
+        "balance": "3333"
+        }
+       ]
+    }
+  ]
+}
+
+
+export const mockup_data_1 = {
+                 "address": "erd1jjn3sf4m439850wdnse6svlw6cu5jnxjhlejxax3pfj2lc8vyw6qcwk7ur",
+                 "network": "devnet",
+                 "versions": [
+                   {
+                     "balance": "1000000000000000000",
+                     "nonce": 0,
+                     "tokens": [
+                       {
+                         "balance": "1000000000000000000",
+                         "identifier": "EGLD",
+                         "name": "Native EGLD token"
+                       }
+                     ],
+                     "tx_hash": "69e8a073b4152c41191cf635f494000d15d5ff986156f257ffdc848024563d86",
+                     "version": 1
+                   }
+                 ]
+               }
